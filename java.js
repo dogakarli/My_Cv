d68413005b4f79f78e51b930d7c5eedcbd835bea
Doga Karli <karlidoga0@gmail.com>
2024-05-13 20:59:05 +0300

    my.cv

---
+++ java.js
@@ -0,0 +1,10 @@
+const cizgi = document.querySelector('.cizgi');
+const mobilenav = document.querySelector('.mobilenav');
+
+
+
+
+
+cizgi.addEventListener('click',()=>{
+    mobilenav.classList.toggle('opensidebar')
+});
